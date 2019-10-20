f9e8c79b5c845b3726a047e7f0866ad41be197c0
CycloPi <venti3d@gmail.com>
2019-10-20 20:55:53 +0200

    Ajustes de  menú en el  menú entrar

--- Plantilla_menu/Ejecutable/Menu/Menu_entrar/Menu_entrar.gd
+++ Plantilla_menu/Ejecutable/Menu/Menu_entrar/Menu_entrar.gd
@@ -1,14 +1,10 @@
 extends Node
-#var MenuEntrar = preload(
-#var MenuPrin = preload("res://Ejecutable/Menus/Menu Principal/MenuPrincipal.tscn")
-#var MenuPrincipal = MenuPrin.instance()
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-var yy
 
+var preMenu_niveles = preload("res://Ejecutable/Menu/Menu_niveles/Escena_menu_niveles.tscn")
+var Menu_niveles
 # Called when the node enters the scene tree for the first time.
 func _ready():
+#	print(Menu_niveles)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,6 +13,9 @@ func _ready():
 
 
 func _on_Entrar_pressed():
-	yy =	get_tree().change_scene("res://Ejecutable/Juego/Escenarios/Niveles/Nivel1/Nivel1.tscn")
-	print(yy, "  esto que coño es")
+#	valor01 =	get_tree().change_scene("res://Ejecutable/Juego/Escenarios/Niveles/Nivel1/Nivel1.tscn")
+	Menu_niveles =preMenu_niveles.instance()
+	get_node("/root/Nodo_Dios").add_child(Menu_niveles)
+	$".".queue_free()
+	print(get_path())
 	pass # Replace with function body.
