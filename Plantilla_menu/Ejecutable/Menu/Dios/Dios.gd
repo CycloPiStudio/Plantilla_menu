2053539314d5bf351d4525fe4ffed0cb7cd7b814
CycloPi <venti3d@gmail.com>
2019-10-20 20:53:32 +0200

    Ajustes de  menú en la escena dios

--- Plantilla_menu/Ejecutable/Menu/Dios/Dios.gd
+++ Plantilla_menu/Ejecutable/Menu/Dios/Dios.gd
@@ -6,6 +6,8 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$"Music_menu".play()
+
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
