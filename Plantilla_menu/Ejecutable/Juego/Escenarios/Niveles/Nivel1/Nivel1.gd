57b585cf669b648df7088ed05a0213cbe49ef115
CycloPi <venti3d@gmail.com>
2019-10-21 17:24:25 +0200

    contador de segundos

--- Plantilla_menu/Ejecutable/Juego/Escenarios/Niveles/Nivel1/Nivel1.gd
+++ Plantilla_menu/Ejecutable/Juego/Escenarios/Niveles/Nivel1/Nivel1.gd
@@ -2,20 +2,15 @@ extends Node2D
 
 var preMenu = load("res://Ejecutable/Menu/Menu_niveles/Escena_menu_niveles.tscn")
 var Menu
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+var My_timer = 0
 
 func _on_Timer_timeout():
-	Menu =preMenu.instance()
-	get_node("/root/Nodo_Dios").add_child(Menu)
-	get_node("/root/Nodo_Dios/Music_menu").play()
-	$".".queue_free()
-	
+	My_timer += 1
+	get_node("Label").set_text(str(3-My_timer))
+	if My_timer > 3:
+		Menu =preMenu.instance()
+		get_node("/root/Nodo_Dios").add_child(Menu)
+		get_node("/root/Nodo_Dios/Music_menu").play()
+		$".".queue_free()
+	get_node("Timer").start()
 	pass # Replace with function body.
